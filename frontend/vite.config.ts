4f9949b194fc7dffc8e0312d6e0f6f15e6353252
vsevolod.zolotov <vsevolod.zolotov@bimeister.com>
2022-03-05 12:01:38 +0300

    feature: added pwa support

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,37 @@ import { fileURLToPath, URL } from "url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import {VitePWA} from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),     VitePWA({
+    includeAssets: ['favicon.ico', 'robots.txt', 'img/icons/apple-touch-icon.png'],
+    manifest: {
+      name: 'Liza Alert Map',
+      short_name: 'LAMAP',
+      description: 'Liza Alert Map',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'img/icons/192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'img/icons/512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: 'img/icons/maskable-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable',
+        }
+      ]
+    }
+  })],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
